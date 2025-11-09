5f8afa36c915557080672ade6cb7081fc75ab850
r4tur1 <aadityaraturi11@gmail.com>
2025-11-09 07:25:49 +0000

    update

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjYFncY5mlxjGkPQm7jzguRnrPTKJUWqI",
+  authDomain: "diskengrabr4tur1.firebaseapp.com",
+  projectId: "diskengrabr4tur1",
+  storageBucket: "diskengrabr4tur1.firebasestorage.app",
+  messagingSenderId: "92656242137",
+  appId: "1:92656242137:web:ef10e0f246a1c1b6335dc8",
+  measurementId: "G-JX09PNQDCV"
+};
