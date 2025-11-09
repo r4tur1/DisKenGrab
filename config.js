ae1ba39779bf3f33b377edff25d4e2fb520d64cd
r4tur1 <aadityaraturi11@gmail.com>
2025-11-09 06:05:19 +0000

    Remove config.js from repo and add to gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-    const firebaseConfig = {
-        apiKey: "AIzaSyCkGaOYfoY-27AT_yGL6aETxH6S8VlA0A8",
-        authDomain: "diskengrab.firebaseapp.com",
-        projectId: "diskengrab",
-        storageBucket: "diskengrab.firebasestorage.app",
-        messagingSenderId: "768269261155",
-        appId: "1:768269261155:web:36dd8910fa86d86d851b56",
-        measurementId: "G-WER9ES8S7Q"
-    };
